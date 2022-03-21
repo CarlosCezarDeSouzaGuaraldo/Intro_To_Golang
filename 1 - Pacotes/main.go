f99f118e40241a16e3de04d91bf9ce57ba294fc7
Carlos Cezar De Souza <carlos@codebit.com.br>
2022-03-21 13:43:28 -0300

    Working with external modules in Go

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Writing in main file")
 	auxiliar.Write()
+
+	verifyngEmail := checkmail.ValidateFormat("carloscezardesouza@outlook.com")
+	fmt.Println(verifyngEmail)
 }
