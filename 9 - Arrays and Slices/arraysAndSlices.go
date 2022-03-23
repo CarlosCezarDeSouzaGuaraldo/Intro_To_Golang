bd7c066a5cae7ead5b19ecd7ac612d68b262a8dd
Carlos Cezar De Souza <carlos@codebit.com.br>
2022-03-23 00:55:46 -0300

    Working with arrays and slices in GO

---
+++ 9 - Arrays and Slices/arraysAndSlices.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	fmt.Println("Arrays and Slices")
+
+	var array1 [5]string
+	array1[0] = "First position"
+	fmt.Println(array1)
+
+	array2 := [5]string{"First position", ""}
+	fmt.Println(array2)
+
+	array3 := [...]int{1,2,3,4,5}
+	fmt.Println(array3)
+
+	slice := []int{1,2,3}
+	slice = append(slice, 4)
+	fmt.Println(slice)
+
+	slice2 := array3[1:3]
+	fmt.Println(slice2)
+
+	array3[1] = 12
+	fmt.Println(slice2)
+
+	fmt.Println(reflect.TypeOf(slice))
+	fmt.Println(reflect.TypeOf(array3))
+}
